293e7cf83dc5b46eef0e3f2791aa18210d2fff9b
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-04-29 14:38:18 -0300

    post to api from form

--- lib/app.rb
+++ lib/app.rb
@@ -1,10 +1,13 @@
 require 'sinatra'
+require 'sinatra/json'
 require 'sinatra_more/markup_plugin'
 
 register SinatraMore::MarkupPlugin
 
 post '/login' do
   puts params
+  status 200
+  json message: 'Success'
 end
 
 get '*' do
