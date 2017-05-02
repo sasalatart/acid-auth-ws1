8731a43325a9b17a0cf9b0346caa215cd974b09e
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-05-02 18:46:05 -0300

    fix Faraday crash

--- lib/services/ws2_connection.rb
+++ lib/services/ws2_connection.rb
@@ -2,8 +2,14 @@ require 'faraday'
 
 module WS2Connection
   def self.authenticate(email, base64_image)
-    conn = Faraday.new(url: ENV['WS2_URL']) { |farad| farad.response :logger }
-    response = conn.post "/verify_user/#{email}", image: base64_image
-    { status: response.status, message: response.message }
+    conn = Faraday.new(url: ENV['WS2_URL']) do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
+    end
+
+    response = conn.post "/users/verify/#{email}", image: base64_image
+
+    { status: response.status, message: response.body['message'] }
   end
 end
